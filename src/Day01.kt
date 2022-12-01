966c9e4c02265174cfc2d07d7711181e4fbd20ba
Marcin Mrugas <marcin.mrugas@allegro.pl>
2022-12-01 09:48:44 +0100

    Solution for Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
+
+private typealias Group = MutableList<List<Int>>
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: Group): Int {
+        return input.maxOfOrNull {
+                it.sum()
+            } ?: throw Exception("No solution found")
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: Group): Int {
+        return input
+            .map { it.sum() }
+            .sorted()
+            .takeLast(3)
+            .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    var group = mutableListOf<Int>()
     val input = readInput("Day01")
+        .map { it.toIntOrNull() }
+        .foldRight(mutableListOf<List<Int>>()) { item, acc ->
+            if (item == null) {
+                acc.add(group)
+                group = mutableListOf()
+            } else {
+                group.add(item)
+            }
+            acc
+        }
     println(part1(input))
     println(part2(input))
 }
