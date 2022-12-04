e33e34d0122aec4bd3412eb00d222e264515d025
Marcin Mrugas <marcin.mrugas@allegro.pl>
2022-12-04 11:06:58 +0100

    Solution for day 4

---
+++ src/Day04.kt
@@ -0,0 +1,36 @@
+private fun IntRange.contains(other: IntRange): Boolean {
+    return first <= other.first && last >= other.last
+}
+
+private fun IntRange.overlap(other: IntRange): Boolean {
+    return first <= other.last && other.first <= last
+}
+
+fun main() {
+    fun parse(line: String): List<IntRange> {
+        return line.split(',').map { segment ->
+            val (a,b) = segment.split('-').map { it.toInt() }
+            a..b
+        }
+    }
+
+    fun part1(input: List<List<IntRange>>): Int {
+        return input.filter { (a,b) ->
+            a.contains(b) || b.contains(a)
+        }.size
+    }
+
+    fun part2(input: List<List<IntRange>>): Int {
+        return input.filter { (a,b) ->
+            a.overlap(b)
+        }.size
+    }
+
+    val testInput = readInput("Day04_test").map { parse(it) }
+    println(part1(testInput))
+    println(part2(testInput))
+
+    val input = readInput("Day04").map { parse(it) }
+    println(part1(input))
+    println(part2(input))
+}
