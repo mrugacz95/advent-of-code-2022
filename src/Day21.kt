8f76603e1caeca9ba9b355c3ec8000fc6ed2c77b
Marcin Mrugas <marcin.mrugas@allegro.pl>
2022-12-29 21:48:01 +0100

    Refactor for part 1 in day 21

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,42 +1,58 @@
-private sealed class ParseTree
-private data class Terminal(val value: Int) : ParseTree()
-private data class Production(var job1: String, var job2: String, val symbol: Char) : ParseTree()
+private sealed class TreeNode(open val name: String)
+private data class Terminal(
+    val value: Int,
+    override val name: String
+) : TreeNode(name)
+
+private data class Production(
+    var lhs: TreeNode,
+    var rhs: TreeNode,
+    val symbol: Char,
+    override val name: String
+) : TreeNode(name)
 
 private const val ROOT = "root"
 
 fun main() {
-    fun List<String>.parse(): Map<String, ParseTree> {
-        val tree = mutableMapOf<String, ParseTree>()
-        for (line in this) {
-            val key = line.slice(0..3)
-            val value = line.slice(6 until line.length)
-            val operation = if (" " in value) {
-                val job1 = line.slice(6..9)
-                val job2 = line.slice(13..16)
-                val symbol = line[11]
-                Production(job1, job2, symbol)
-            } else {
-                Terminal(value.toInt())
-            }
-            tree[key] = operation
+    fun List<String>.parse(): TreeNode {
+
+        val tree = this.associate {
+            val key = it.slice(0..3)
+            val value = it.slice(6 until it.length)
+            key to value
         }
-        return tree
+
+        fun parseSubTree(nodeName: String): TreeNode {
+            val value = tree[nodeName] ?: error("Node not found")
+            return if (!value.first().isDigit()) {
+                val lhsNodeName = value.slice(0..3)
+                val rhsNodeName = value.slice(7..10)
+                val lhs = parseSubTree(lhsNodeName)
+                val rhs = parseSubTree(rhsNodeName)
+                val symbol = value[5]
+                Production(lhs, rhs, symbol, nodeName)
+            } else {
+                Terminal(value.toInt(), nodeName)
+            }
+        }
+
+
+        return parseSubTree(ROOT)
     }
 
-    fun evaluateNode(name: String, tree: Map<String, ParseTree>): Long {
-        val node = tree[name]
-        when (node) {
-            is Terminal -> return node.value.toLong()
+    fun evaluateNode(tree: TreeNode): Long {
+        when (tree) {
+            is Terminal -> return tree.value.toLong()
             is Production -> {
-                val operation: (Long, Long) -> Long = when (node.symbol) {
+                val operation: (Long, Long) -> Long = when (tree.symbol) {
                     '+' -> { a, b -> a + b }
                     '*' -> { a, b -> a * b }
                     '/' -> { a, b -> a / b }
                     '-' -> { a, b -> a - b }
                     else -> error("Unknown operation")
                 }
-                val ev1 = evaluateNode(node.job1, tree)
-                val ev2 = evaluateNode(node.job2, tree)
+                val ev1 = evaluateNode(tree.lhs)
+                val ev2 = evaluateNode(tree.rhs)
                 return operation(ev1, ev2)
             }
 
@@ -48,12 +64,12 @@ fun main() {
 
     fun part1(input: List<String>): Long {
         val tree = input.parse()
-        return evaluateNode(ROOT, tree)
+        return evaluateNode(tree)
     }
 
     fun part2(input: List<String>): Int {
-        val tree = input.parse().toMutableMap()
-        return tree.size
+        val tree = input.parse()
+        return 0
     }
 
     val testInput = readInput("Day21_test")
